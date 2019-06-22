322251f29de461efc90b5aa4ac2e413d022abbaf
ghtea <47841931+ghtea@users.noreply.github.com>
2019-06-22 09:54:30 +0900

    Update menu_jp.js

--- JP/menu_jp.js
+++ JP/menu_jp.js
@@ -1,3 +1,3 @@
-$(document).ready(function () {
-    $("#menu").load("https://ghtea.github.io/hots/JP/menu_jp.html");
+$(function(){
+  $("#menu").load("https://ghtea.github.io/hots/JP/menu_jp.html");
 });
