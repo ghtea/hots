6b16a1e6fc3ea14caeb5481af96fde29494122b0
ghtea <47841931+ghtea@users.noreply.github.com>
2019-06-23 21:12:05 +0900

    1

---
+++ my_javascript.js
@@ -0,0 +1,22 @@
+
+ //call my menu !!!
+   $(function(){
+ $("#menu_div").load("https://ghtea.github.io/hots/JP/menu_jp.html");
+    });
+  
+//make only one menu clickable
+function onlyOne(checkbox) {
+    var checkboxes = document.getElementsByName('menu');
+    checkboxes.forEach((item) => {
+        if (item !== checkbox) item.checked = false
+    })
+  }
+
+//
+function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
