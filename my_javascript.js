bef49b2f19b2a0bdc801acb7394707b49a69e397
ghtea <47841931+ghtea@users.noreply.github.com>
2019-06-25 04:47:18 +0900

    1

--- my_javascript.js
+++ my_javascript.js
@@ -16,7 +16,7 @@ function onlyOne(checkbox) {
 //
 function toggle_visibility(id) {
        var e = document.getElementById(id);
-       if(e.style.display == 'none')
+       if(e.style.display == 'block')
           e.style.display = 'block';
        else
           e.style.display = 'none';
